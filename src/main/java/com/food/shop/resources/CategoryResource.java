3890f7359be8d64e84f67ba2b776f32d09d29e03
Alberto Paiva <dojathan@gmail.com>
2021-05-30 18:57:48 -0300

    Created resource GET produtos/id

--- src/main/java/com/food/shop/resources/CategoryResource.java
+++ src/main/java/com/food/shop/resources/CategoryResource.java
@@ -1,9 +1,11 @@
 package com.food.shop.resources;
 
 import com.food.shop.domain.Category;
+import com.food.shop.domain.Product;
 import com.food.shop.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +23,11 @@ public class CategoryResource {
     public ResponseEntity<List<Category>> findAll() {
         List<Category> categories = categoryService.findAll();
         return ResponseEntity.ok(categories);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Category> findById(@PathVariable Integer id) {
+        Category cat = categoryService.findById(id);
+        return ResponseEntity.ok(cat);
     }
 }
--- src/main/java/com/food/shop/services/CategoryService.java
+++ src/main/java/com/food/shop/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.food.shop.services;
 
 import com.food.shop.domain.Category;
+import com.food.shop.domain.Product;
 import com.food.shop.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -16,5 +18,10 @@ public class CategoryService {
     public List<Category> findAll() {
         List<Category> categories = categoryRepository.findAll();
         return categories;
+    }
+
+    public Category findById(Integer id) {
+        Optional<Category> prod = categoryRepository.findById(id);
+        return prod.orElseThrow(() -> new RuntimeException("Categoria não encontrado!"));
     }
 }
